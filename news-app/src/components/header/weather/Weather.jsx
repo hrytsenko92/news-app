df27c8a6977fa99e5456bd96ec0e33141530094f
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-07 17:11:16 +0300

    add my location function

--- news-app/src/components/header/weather/Weather.jsx
+++ news-app/src/components/header/weather/Weather.jsx
@@ -2,6 +2,7 @@ import style from "./weather.module.css";
 import sunImg from "./sun.svg";
 import rainImg from "./rain.svg";
 import cloudyImg from "./cloudy.svg";
+import { createFactory } from "react";
 
 const Weather = () => {
   return (
@@ -17,96 +18,33 @@ const Weather = () => {
   );
 };
 
-const getWeather = () => {
-    let options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
+
+    const getWeather = () => {
+      const onSuccess = (position) => {
+       let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`
+       fetch(weatherUrl)
+        .then((resp) => {return resp.json() 
+        .then((data) => {console.log(data); return data}); });
       };
-      
-      function success(pos) {
-        let crd = pos.coords;
- 
-        console.log(`Широта: ${crd.latitude}`);
-        console.log(`Долгота: ${crd.longitude}`);
-        console.log(`Плюс-минус ${crd.accuracy} метров.`);
+      const onError = () => {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${50.449755545630815}&lon=${30.525695923164733}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`
+        ).then((resp) => {
+          return resp.json()
+          .then((data) => {
+            console.log(data);
+          });
+        });
       };
-      
-      function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
+      const getGeoData = () => {
+        let aaa = navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        console.log(aaa)
+        
       };
+      getGeoData();
       
-      navigator.geolocation.getCurrentPosition(success, error, options);
- 
+    };
+    console.log ("fin     =     " + getWeather());
 
-//   getMyLocation();
-};
-
-/////
-getWeather();
-//////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const getMyLocation = () => {
-//   let userLatitude = null;
-//   let userLongitude = null;
-//   let success = (position) => {
-//     userLatitude = position.coords.latitude;
-//     userLongitude = position.coords.longitude;
-//   };
-
-//   let err = () => {
-//     let geolocationdata = false;
-//     return geolocationdata;
-//   };
-
-//   let geolocationdata = navigator.geolocation.getCurrentPosition(success, err);
-//     console.log(userLatitude);
-
-// //   fetch(
-// //     `https://api.openweathermap.org/data/2.5/weather?lat=${userLatitude}&lon=${userLongitude}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`
-// //   );
-
-// //   return result;
-// };
-
-// let geoData = getMyLocation();
-// // console.log(geoData);
 
 export default Weather;
