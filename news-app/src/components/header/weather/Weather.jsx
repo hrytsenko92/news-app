c8bcc1e5583347d2f9b5e40b72f6aab89ac730a1
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-09 21:06:18 +0300

    add weather jsx

--- news-app/src/components/header/weather/Weather.jsx
+++ news-app/src/components/header/weather/Weather.jsx
@@ -1,27 +1,32 @@
 import { getUserWeather } from "./getWeather";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import style from "./weather.module.css";
 import sunImg from "./sun.svg";
 import rainImg from "./rain.svg";
 import cloudyImg from "./cloudy.svg";
-import { createFactory } from "react";
 
 const Weather = () => {
-  const getMyWeather = async () => {
-    const weatherData = await getUserWeather().then((res) => {console.log(res); return res});
-    console.log(weatherData.main);
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const getMyWeather = async () => {
+    const weatherData = await getUserWeather().then((res) => {return res;}).then((res) => setCurrentWeather(res));
   };
 
   useEffect(() => {
     getMyWeather();
   }, []);
+
+  console.log(currentWeather);
+
   return (
     <div className={style.weather}>
       <div className="container">
         <div className="info">
-          <div className="temperature">{}</div>
+          <div className="temperature"></div>
           <div className="windSpeed"></div>
           <div className="humidity"></div>
+        </div>
+        <div>
+          {/* {currentWeather} */}
         </div>
         <img src="" alt="weatherImg" className="weatherImg" />
       </div>
