53ed781c895dd596076a46c7b54679c795b11a48
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-09 14:06:39 +0300

    fix getWeather

--- news-app/src/components/header/weather/Weather.jsx
+++ news-app/src/components/header/weather/Weather.jsx
@@ -1,3 +1,5 @@
+import { getUserWeather } from "./getWeather";
+import { useEffect } from "react";
 import style from "./weather.module.css";
 import sunImg from "./sun.svg";
 import rainImg from "./rain.svg";
@@ -5,46 +7,26 @@ import cloudyImg from "./cloudy.svg";
 import { createFactory } from "react";
 
 const Weather = () => {
+  const getMyWeather = async () => {
+    const weatherData = getUserWeather();
+    console.log("sqqq        " + weatherData);
+  };
+
+  useEffect(() => {
+    getMyWeather();
+  }, []);
   return (
     <div className={style.weather}>
       <div className="container">
         <div className="info">
-          <div className="temperature">100dg</div>
+          <div className="temperature">{}</div>
           <div className="windSpeed"></div>
+          <div className="humidity"></div>
         </div>
         <img src="" alt="weatherImg" className="weatherImg" />
       </div>
     </div>
   );
 };
-
-
-    const getWeather = () => {
-      let result = null;
-      const onSuccess = async (position) => {
-        let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`
-        let res = await fetch(weatherUrl).then((resp) => {return resp.json().then((data) => {console.log(data); return data}); });
-        console.log(res);
-      };
-      
-      const onError = () => {
-        fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${50.449755545630815}&lon=${30.525695923164733}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`
-        ).then((resp) => {
-          return resp.json()
-          .then((data) => {
-            console.log(data);
-          });
-        });
-      };
-      const getGeoData = () => {
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
-      };
-      getGeoData();
-      console.log(result);
-      return result;
-    };
-    console.log ("fin     =     " + getWeather());
-
 
 export default Weather;
