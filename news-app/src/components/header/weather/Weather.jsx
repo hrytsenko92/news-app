31e9b6b15dd481a8c33831db09e049a377731c8b
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-10 21:17:11 +0300

    fix getWeather

--- news-app/src/components/header/weather/Weather.jsx
+++ news-app/src/components/header/weather/Weather.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import style from "./weather.module.css";
 import sunImg from "./sun.svg";
 import rainImg from "./rain.svg";
 import cloudyImg from "./cloudy.svg";
 
 const Weather = () => {
-  const [currentWeather, setCurrentWeather] = useState([]);
-   const getUserWeather = async () => {
-    try {
-        let userCoords = await new Promise(function (resolve, reject) {return navigator.geolocation.getCurrentPosition(resolve, reject);});
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  useEffect(() => {
+    const getUserWeather = async () => {
+      try {
+        let userCoords = await new Promise(function (resolve, reject) {return navigator.geolocation.getCurrentPosition(resolve, reject)});
         let response = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${userCoords.coords.latitude}&lon=${userCoords.coords.longitude}&appid=9a564f272063217be589d2a7938b8c8e&units=metric`)
-        .then((resp) => {return resp.json()})
-        .then((res) => {console.log(res)})
-        return response; ////
-    } catch (error) {
-        console.error("Problem in:", error.message)
-    }
-};
-  ///   .then((res) => {console.log(res); setCurrentWeather(res)})
-  useEffect(
-    () => {
+          .then((resp) => {return resp.json()})
+          .then((resp) => {console.log(resp); setCurrentWeather(resp) })
+        return response; 
+      } catch (error) {
+        console.error("Problem in:", error.message);
+      }
+    };
     getUserWeather();
   }, []);
+  console.log(currentWeather);
   return (
     <div className={style.weather}>
       <div className="container">
