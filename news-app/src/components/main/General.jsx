e693ab7987847302af1dafd8429c23227d62c24f
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-13 14:43:53 +0300

    fix router

--- news-app/src/components/main/General.jsx
+++ news-app/src/components/main/General.jsx
@@ -1,4 +1,23 @@
+import React, {useState} from "react";
+import axios from "axios";
+import './general.module.css';
+
+
+
+
 const General = () => {
+  [articles, setArticles] = useState([]);
+
+
+
+
+
+
+
+
+
+
+
     return (
       <div className='general'>
 <span>general</span>
--- news-app/src/components/main/Main.jsx
+++ news-app/src/components/main/Main.jsx
@@ -1,33 +1,35 @@
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
-import style from './main.module.css'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import style from "./main.module.css";
 
-import General from './General';
-import Sport from './Sport';
-import Health from './Health'
-import Cars from './Cars';
-import Economics from './Economics';
-
-
+import General from "./General";
+import Sport from "./Sport";
+import Health from "./Health";
+import Cars from "./Cars";
+import Economics from "./Economics";
 
 const Main = () => {
-    return (
-      <div className={style.main}>
+  return (
+    <div className={style.main}>
+      <nav className="navigation">
         <Router>
-                <li><Link to="./General">General</Link></li>
-                <li><Link to="./Sport">Sport</Link></li>
-                <li><Link to="./Health">Health</Link></li>
-                <li><Link to="./Cars">Cars</Link></li>
-                <li><Link to="./Economics">Economics</Link></li>
-            <Routes>
-                <Route path="./General" component={General}/>
-                <Route path="./Sport" component={Sport}/>
-                <Route path="./Health" component={Health}/>
-                <Route path="./Cars" component={Cars}/>
-                <Route path="./Economics" component={Economics}/>
-            </Routes>
+          <ul className="routerNav">
+            <li className="routerNavItem"><Link to="./General">General</Link></li>
+            <li className="routerNavItem"><Link to="./Sport">Sport</Link></li>
+            <li className="routerNavItem"><Link to="./Health">Health</Link></li>
+            <li className="routerNavItem"><Link to="./Cars">Cars</Link></li>
+            <li className="routerNavItem"><Link to="./Economics">Economics</Link></li>
+          </ul>
+          <Routes>
+            <Route path="/General" element={<General/>} />
+            <Route path="/Sport" element={<Sport/>} />
+            <Route path="/Health" element={<Health/>} />
+            <Route path="/Cars" element={<Cars/>} />
+            <Route path="/Economics" element={<Economics/>} />
+          </Routes>
         </Router>
-      </div>
-    )
-  }
-  
-  export default Main;
+      </nav>
+    </div>
+  );
+};
+
+export default Main;
