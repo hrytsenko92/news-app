c4058e494a0af60bdb3d86bf82ee74a1c3476e21
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-16 20:33:56 +0300

    fix axios response

--- news-app/src/components/main/Main.jsx
+++ news-app/src/components/main/Main.jsx
@@ -6,9 +6,11 @@ import style from "./main.module.css";
 const Main = () => {
   const [articles, setArticles] = useState([]);
   const [articleCategory, setArticleCategory] = useState("science");
+  console.log(articles);
 
   const [loading, setLoading] = useState(false);
 
+  const setCategory = (str) => setArticleCategory(str);
   const pageCategory = {
     pageScience : "science",
     pageSports : "sports",
@@ -22,12 +24,15 @@ const Main = () => {
       setLoading(false);
     };
     getArticles();
-  }, []);
+  }, [articleCategory]);
   return (
     <div className='Main'>
-      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageScience)}></button>
-      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageSports)}></button>
-      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageHealth)}></button>
+      <button className={style.btnChangeCategory} onClick={()=> {setCategory(pageCategory.pageScience)}}></button>
+      <button className={style.btnChangeCategory} onClick={()=> {setCategory(pageCategory.pageSports)}}></button>
+      <button className={style.btnChangeCategory} onClick={()=> {setCategory(pageCategory.pageHealth)}}></button>
+      <div>{articles.map((articles) =>
+        <span key={articles.publishedAt}>{articles.description}</span>
+      )}</div>
     </div>
   );
 }
