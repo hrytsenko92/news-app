9f90467f333fb94b780ad95d61d11388a6020462
hrytsenko92 <m.hrytsenko1820@gmail.com>
2022-08-16 19:29:53 +0300

    delete sourseitem jsx

--- news-app/src/components/main/Main.jsx
+++ news-app/src/components/main/Main.jsx
@@ -1,29 +1,19 @@
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import style from "./main.module.css";
-import SourceItem from './SourceItem';
+
 
 const Main = () => {
   const [articles, setArticles] = useState([]);
-  const [articleCategory, setArticleCategory] = useState("science"); ///// default 
+  const [articleCategory, setArticleCategory] = useState("science");
 
   const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [articlePage] = useState(10);
 
-  const lastArticleIndex = currentPage * articlePage;
-  const firstArticleIndex = lastArticleIndex - articlePage;
-  const currentArticle = articles.slice(firstArticleIndex, lastArticleIndex);
-
-  const setUserCategory = (str) => {setArticleCategory(str)}; /// console.log("setUser:   " + str); 
-
-  const contentCategoryData = {
+  const pageCategory = {
     pageScience : "science",
     pageSports : "sports",
     pageHealth : "health",
   }
-
   useEffect(() => {
     const getArticles = async () => {
       setLoading(true);
@@ -34,24 +24,12 @@ const Main = () => {
     getArticles();
   }, []);
   return (
-    <div className={style.main}>
-      <nav className="navigation">
-        <Router>
-          <ul className="routerNav">
-            <li className="routerNavItem"><Link to="./SourceItem1">Science</Link></li>
-            <li className="routerNavItem"><Link to="./SourceItem2">Sport</Link></li>
-            <li className="routerNavItem"><Link to="./SourceItem3">Health</Link></li>
-          </ul>
-          <Routes>
-            <Route path="/" element={<Navigate replace to="/SourceItem1"/>} />
-            <Route exact path="/SourceItem1" element={<SourceItem articleData={articles} category={contentCategoryData.pageScience} UserCategory={setUserCategory} />} />
-            <Route exact path="/SourceItem2" element={<SourceItem articleData={articles} category={contentCategoryData.pageSports}  UserCategory={setUserCategory} />} />
-            <Route exact path="/SourceItem3" element={<SourceItem articleData={articles} category={contentCategoryData.pageHealth}  UserCategory={setUserCategory} />} />
-          </Routes>
-        </Router>
-      </nav>
+    <div className='Main'>
+      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageScience)}></button>
+      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageSports)}></button>
+      <button className={style.btnChangeCategory} onClick={setArticleCategory(pageCategory.pageHealth)}></button>
     </div>
   );
-};
+}
 
 export default Main;
